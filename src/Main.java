292abe62786778ea1222c3f772a0fcdf72fd28b0
Elena_An <e.anikey@gmail.com>
2022-02-06 20:06:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9850;
+        int mile = 20;
+        int totalMile = ticket / mile;
+        System.out.println(totalMile);
+    }
+}
